e8d6d5d0ddfedaace96c8b3f811691f92e0c426b
Victor <victoriglcue@gmail.com>
2019-02-20 19:44:00 +0100

    Creado index.html y ficheros con las familias de productos. Bien linkados entre ellos. Faltan las imagenes de los productos según familia

---
+++ Practica-1/server-image.js
@@ -0,0 +1,63 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+console.log("Arrancando servidor...")
+
+//-- Configurar y lanzar el servidor. Por cada peticion recibida
+//-- se imprime un mensaje en la consola
+http.createServer((req, res) => {
+  console.log("---> Peticion recibida")
+  console.log("Recurso solicitado (URL): " + req.url)
+  var q = url.parse(req.url, true);
+  console.log("URL parseada: ")
+  console.log("Host: " + q.host)
+  console.log("pathname:" + q.pathname)
+
+  //-- Obtener el fichero. Si es "/" se toma index.html
+  //-- Poner el "." delante para que sean un fichero del directorio actual
+
+  var filename = ""
+
+  if (q.pathname == "/")
+    filename += "/index.html"
+  else {
+    filename = q.pathname
+  }
+
+  //-- Obtener el tipo de fichero segun la extension
+  tipo = filename.split(".")[1]
+
+  //-- Obtener el nombre del fichero a partir del recurso solicitado
+  //-- Se añade un . delante
+  filename = "." + filename
+
+  console.log("Filename: " + filename)
+  console.log("Tipo: " + tipo)
+
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+
+    //-- Tipo mime por defecto: html
+    var mime = "text/html"
+
+    //-- Es una imagen
+    if (['png', 'jpg'].includes(tipo)) {
+      console.log("IMAGEN!!!!!")
+      mime = "image/" + tipo
+    }
+
+    //-- Es un css
+    if (tipo == "css")
+      mime = "text/css"
+
+    //-- Generar el mensaje de respuesta
+    res.writeHead(200, {'Content-Type': mime});
+    res.write(data);
+    res.end();
+  });
+
+}).listen(8080);
